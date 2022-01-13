4d78f09985d21746e77392aeb92d79c222835cdf
jjary <94367829+jjary@users.noreply.github.com>
2022-01-13 14:45:34 +0100

    Test section - finished header and footer (#35)
    * kicking of fucking shit
    
    * modification of index.html to usefull status
    
    * modification of newsletter styles, sill not working
    
    * Modification of styles in index, added variables etc.
    
    * modified styles of header, start, features, footer, next step of own styles for buttons and inputs, features adapted in 50%
    
    * small modification in start
    
    * styles of inputs and buttons updated, newsletter and features adapted
    
    * Merging to update
    
    * Merging of css.map
    
    * changes in buttons, start the optimilizing of features, few modifications in start and footer
    
    * functions changed to features, functions deleted
    
    * first step of header modification, optimalization of features in 50%, newsletter adapted
    
    * header changed to first vision, few modification in footer, aboutus, start and newsletter
    
    * gitignore actualized
    
    * header completed in 80%
    
    * main.js off
    
    * header finished
    
    * footer finished
    
    Co-authored-by: PufAnimalsDev <93289768+PufAnimalsDev@users.noreply.github.com>

--- js/main.js
+++ js/main.js
@@ -1,27 +1,27 @@
-//start navbar hidding
-window.onscroll = function() {scrollFunctionHide()};
+// //start navbar hidding
+// window.onscroll = function() {scrollFunctionHide()};
 
-function scrollFunctionHide() {
+// function scrollFunctionHide() {
 
-  window.addEventListener("scroll", function (event) {
-  let scrolled = this.scrollY;  
+//   window.addEventListener("scroll", function (event) {
+//   let scrolled = this.scrollY;  
 
-  if (scrolled > 20) {
-    document.getElementById("navHider").style.display = "none";
-  }
-  else {
-    document.getElementById("navHider").style.display = "";
-  }});  
-}
-//end navbar hidding
+//   if (scrolled > 20) {
+//     document.getElementById("navHider").style.display = "none";
+//   }
+//   else {
+//     document.getElementById("navHider").style.display = "";
+//   }});  
+// }
+// //end navbar hidding
 
-/*Start function of toggling navbar*/
-/* Open */
-function openNav() {
-    document.getElementById("myNav").style.height = "100%";
-}  
-/* Close */
-function closeNav() {
-    document.getElementById("myNav").style.height = "0%";
-}
-/*End function of toggling navbar*/
+// /*Start function of toggling navbar*/
+// /* Open */
+// function openNav() {
+//     document.getElementById("myNav").style.height = "100%";
+// }  
+// /* Close */
+// function closeNav() {
+//     document.getElementById("myNav").style.height = "0%";
+// }
+// /*End function of toggling navbar*/
