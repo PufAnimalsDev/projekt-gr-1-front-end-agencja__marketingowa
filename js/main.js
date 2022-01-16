7d2d35b771986d24dd93603f15db98bda0fbc93c
Prezes <nofavrell@gmail.com>
2022-01-16 23:10:32 +0100

    new style of logo and few photos, modified styles of front page and menu, navbar hiding restored with navmenu at top of page

--- js/main.js
+++ js/main.js
@@ -1,27 +1,29 @@
-// //start navbar hidding
-// window.onscroll = function() {scrollFunctionHide()};
+//start navbar hidding
+window.onscroll = function() {scrollFunctionHide()};
 
-// function scrollFunctionHide() {
+function scrollFunctionHide() {
 
-//   window.addEventListener("scroll", function (event) {
-//   let scrolled = this.scrollY;  
+  window.addEventListener("scroll", function (event) {
+  let scrolled = this.scrollY;  
 
-//   if (scrolled > 20) {
-//     document.getElementById("navHider").style.display = "none";
-//   }
-//   else {
-//     document.getElementById("navHider").style.display = "";
-//   }});  
-// }
-// //end navbar hidding
+  if (scrolled > 20) {
+    document.getElementById("navHider").style.display = "none";
+    document.getElementById("scrollMenu").style.display = "block";
+  }
+  else {
+    document.getElementById("navHider").style.display = "";
+    document.getElementById("scrollMenu").style.display = "none";
+  }});  
+}
+//end navbar hidding
 
-// /*Start function of toggling navbar*/
-// /* Open */
-// function openNav() {
-//     document.getElementById("myNav").style.height = "100%";
-// }  
-// /* Close */
-// function closeNav() {
-//     document.getElementById("myNav").style.height = "0%";
-// }
-// /*End function of toggling navbar*/
+/*Start function of toggling navbar*/
+/* Open */
+function openNav() {
+    document.getElementById("myNav").style.height = "100vh";
+}  
+/* Close */
+function closeNav() {
+    document.getElementById("myNav").style.height = "0vh";
+}
+/*End function of toggling navbar*/
