819639da505f5b9a48f2f899820297916d03d193
Jakub Topoliński <topolinskijakub5@gmail.com>
2022-01-23 14:51:01 +0100

    Improve blog styles

--- home.php
+++ home.php
@@ -2,29 +2,41 @@
 
 <section class="">
     <div class="container">
-        <?php if (have_posts()) : ?>
-            <?php while (have_posts()) : ?>
-                <?php the_post(); ?>
-                <div class="row justify-content-center">
-                    <div class="col container">
-                        <article class="blog-post bg-light m-4 p-4">
-
+        <div class="row justify-content-center blog-post-list g-4">
+            <?php if (have_posts()) : ?>
+                <?php while (have_posts()) : ?>
+                    <?php the_post(); ?>
+                    <div class="col-12 col-md-6 col-xl-4">
+                        <article class="blog-post bg-light">
                             <h2 class="blog-post-title"><?= get_the_title() ?></h2>
-                            <p class="blog-post-meta text-muted"><?= the_date() ?> by <a href="<?= get_author_posts_url(get_the_author_meta('ID')); ?>"><?= get_the_author_meta('display_name'); ?></a></p>
+                            <p class="blog-post-meta text-muted"><?= get_the_date() ?> - autor: <a href="<?= get_author_posts_url(get_the_author_meta('ID')); ?>"><?= get_the_author_meta('display_name'); ?></a></p>
+
+                            <a href="<?php the_permalink(); ?>">
+                                <?php the_post_thumbnail(); ?>
+                            </a>
 
                             <p><?= get_the_excerpt() ?></p>
-                            <a href="<?= get_the_permalink(); ?>" class="btn btn-dark">Przeczytaj więcej...</a>
+                            <div class="blog-post-read-more">
+                                <a href="<?= get_the_permalink(); ?>" class="btnOutlineCustom">Przeczytaj więcej...</a>
+                            </div>
+
                         </article>
 
                     </div>
-                </div>
-            <?php endwhile; ?>
+                <?php endwhile; ?>
+        </div>
+        <?php the_posts_pagination(array(
+                    'mid_size'  => 2,
+                    'prev_text' => __('<i class="fas fa-arrow-left"></i>'),
+                    'next_text' => __('<i class="fas fa-arrow-right"></i>'),
+                    'class' => "pagination"
+                )); ?>
 
-        <?php else : ?>
+    <?php else : ?>
 
-            <div>Brak wpisów do wyświetlenia</div>
+        <div>Brak wpisów do wyświetlenia</div>
 
-        <?php endif; ?>
+    <?php endif; ?>
 
     </div>
 </section>
