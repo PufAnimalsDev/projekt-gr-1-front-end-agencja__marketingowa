e4dd62272c3d47fb69ec591c55ad245874b00e90
Jakub Topoliński <topolinskijakub5@gmail.com>
2022-01-23 16:04:21 +0100

    Improve blog styles (#49)

--- single.php
+++ single.php
@@ -5,13 +5,11 @@
 <section class="aboutus">
     <div class="container">
         <div class="row justify-content-center">
-            <div class="col container">
+            <div class="col-12 col-lg-9 col-xl-8 col-xxl-7 container">
                 <article class="blog-post bg-light m-4 p-4">
                     <h2 class="blog-post-title"><?= get_the_title() ?></h2>
-                    <p class="blog-post-meta text-muted"><?= the_date() ?> by <a href="<?= get_author_posts_url(get_the_author_meta('ID')); ?>"><?= get_the_author_meta('display_name'); ?></a></p>
-                    <a href="<?php the_permalink(); ?>">
-                        <?php the_post_thumbnail(); ?>
-                    </a>
+                    <p class="blog-post-meta text-muted"><?= the_date() ?> - autor: <a href="<?= get_author_posts_url(get_the_author_meta('ID')); ?>"><?= get_the_author_meta('display_name'); ?></a></p>
+                    <?php the_post_thumbnail(); ?>
                     <p class=""> <?php the_content(); ?></p>
                 </article>
             </div>
